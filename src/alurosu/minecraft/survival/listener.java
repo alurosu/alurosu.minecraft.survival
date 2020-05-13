de4a8b68113a8fa8e5ee65a95e55f4566b1b7f9a
alurosu <alurosu@gmail.com>
2020-05-13 19:01:57 +0300

    - version 1.0: /login, /souls

---
+++ src/alurosu/minecraft/survival/listener.java
@@ -0,0 +1,118 @@
+package alurosu.minecraft.survival;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class listener implements Listener{
+	private Map<Player, String> coords = new HashMap<>();
+	private Map<Player, Boolean> isLoggedIn = new HashMap<>();
+	private Map<Player, String> IPqueue = new HashMap<>();
+	
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+    	Player p = event.getPlayer();
+    	String[] parts = p.getAddress().toString().split(":");
+    	
+    	if (IPqueue.containsValue(parts[0])) {
+        	p.kickPlayer("Anti-flood: please wait a few seconds for the other account to login successfully.");
+    	} else {
+        	IPqueue.put(p, parts[0]);
+        	isLoggedIn.put(p, false);
+        	coords.put(p, p.getLocation().getX()+"-"+p.getLocation().getY()+"-"+p.getLocation().getZ());
+        	p.setInvulnerable(true);
+    	}
+    }
+    
+    @EventHandler
+    public void onPlayerQuitEvent(PlayerQuitEvent event) {
+    	Player p = event.getPlayer();
+    	isLoggedIn.remove(p);
+    	coords.remove(p);
+    	IPqueue.remove(p);
+    }
+    
+    @EventHandler
+    public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
+    	String m = event.getMessage();
+    	if (m.contains("admin") || m.contains("owner")) {
+        	event.getPlayer().sendMessage("Contact the admin on Discord. You can find the invitation link at §aamongdemons.com");
+    	} else if (m.contains("kit")) {
+        	event.getPlayer().sendMessage("You can get §6kits §fwith §bsouls §fat §aamongdemons.com");
+    	}
+    }
+    
+    
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+    	Player p = (Player) event.getWhoClicked();
+    	if (!isLoggedIn.get(p)) {
+			loginMessage(p);
+	        event.setCancelled(true);
+    	}
+    }
+    
+    @EventHandler
+    public void onPlayerItemConsumeEvent(PlayerItemConsumeEvent event) {
+    	Player p = event.getPlayer();
+    	if (!isLoggedIn.get(p)) {
+			loginMessage(p);
+	        event.setCancelled(true);
+    	}
+    }
+    
+    @EventHandler
+    public void onPlayerInteractEvent(PlayerInteractEvent event) {
+    	Player p = event.getPlayer();
+    	if (!isLoggedIn.get(p)) {
+    		loginMessage(p);
+	        event.setCancelled(true);
+    	}
+    }
+    
+    @EventHandler
+    public void onPlayerDropItemEvent(PlayerDropItemEvent event) {
+    	Player p = event.getPlayer();
+    	if (!isLoggedIn.get(p)) {
+			loginMessage(p);
+	        event.setCancelled(true);
+    	}
+    }
+    
+    @EventHandler
+    public void onPlayerMoveEvent(PlayerMoveEvent event) {
+    	Player p = event.getPlayer();
+    	if (!isLoggedIn.get(p)) {
+    		String s = event.getTo().getX()+"-"+event.getTo().getY()+"-"+event.getTo().getZ();
+    		if (!s.equals(coords.get(p))) {
+    			loginMessage(p);
+    	        event.setCancelled(true);
+    		}
+    	}
+    }
+    
+    public void doLogin(Player p) {
+    	isLoggedIn.put(p, true);
+    	IPqueue.remove(p);
+    	p.setInvulnerable(false);
+    }
+    
+    public boolean isLoggedIn(Player p) {
+    	return isLoggedIn.get(p);
+    }
+    
+    public void loginMessage(Player p) {
+    	p.sendMessage("/login [pass] or register at §aamongdemons.com");
+    }
+}
---
+++ src/alurosu/minecraft/survival/plugin.java
@@ -0,0 +1,259 @@
+package alurosu.minecraft.survival;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plugin extends JavaPlugin {
+	private String db_user;
+	private String db_pass;
+	private String db_url;
+	
+	static Connection connection;
+	static String help;
+	
+	private listener l = new listener();
+    @Override
+    public void onEnable() {
+		this.saveDefaultConfig();
+    	FileConfiguration config = this.getConfig();
+    	
+    	db_user = config.getString("mysql.user");
+    	db_pass = config.getString("mysql.pass");
+		db_url = "jdbc:mysql://"+config.getString("mysql.url")+":3306/" + config.getString("mysql.name")+"?useSSL=false";
+    	
+        getServer().getPluginManager().registerEvents(l, this);
+        
+		help = "How to use §bsouls§f:";
+		help += "\n/souls §7- shows how many souls you have§f";
+		help += "\n/souls §6give§f [player] [quantity] §7- give souls to another player§f";
+		help += "\n/souls §6buy§f [quantity] §7- trade levels for souls; tax: 1 soul§f";
+		help += "\n/souls §6sell§f [quantity] §7- trade souls for levels; tax: 1 soul§f";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("jdbc driver unavailable!");
+            return;
+        }
+        try {
+            connection = DriverManager.getConnection(db_url,db_user,db_pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Connection getConnection() {
+        try {
+			if (connection == null || connection.isClosed()) {
+			    connection = DriverManager.getConnection(db_url,db_user,db_pass);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return connection;
+    }
+    
+    @Override
+    public void onDisable() {
+        try {
+            if (connection!=null && !connection.isClosed()){ 
+                connection.close();
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	Player p = (Player) sender;
+        if (label.equalsIgnoreCase("login") || label.equalsIgnoreCase("l")) {
+        	if (l.isLoggedIn(p))
+        		p.sendMessage("You are already logged in.");
+        	else {
+        		if (args.length == 0)
+            		p.sendMessage("Please enter your password: /login [pass]");
+            	else {
+    				try {
+    					connection = getConnection();
+    	        		String sql = "SELECT pass, active FROM users WHERE user='"+p.getName()+"'";
+    	        		ResultSet results = connection.prepareStatement(sql).executeQuery();
+    	        		
+    	        		if (results.first()) {
+    	        			if (results.getInt(2) == 0)
+    	        				p.sendMessage("Your account is §6inactive§f. Please contact an admin on discord.");
+    	        			else if (getMD5Hash(args[0]).equals(results.getString(1))) {
+    	                		l.doLogin(p);
+    	                		
+    	                		// give items
+    	                		String items_sql = "SELECT item, count FROM login_items WHERE user='"+p.getName()+"'";
+    	    	        		ResultSet items_results = connection.prepareStatement(items_sql).executeQuery();
+    	    	        		
+    	    	        		String m = "";
+    	    	        		while(items_results.next()){
+    	    	        			p.getInventory().addItem(new ItemStack(Material.getMaterial(items_results.getString(1)), items_results.getInt(2)));
+    	    	        			
+    	    	        			if (m.isEmpty())
+    	    	        				m = "You received: ";
+    	    	        			else
+    	    	        				m += ", ";
+    	    	        			m += "§6"+items_results.getInt(2)+" §f"+items_results.getString(1);
+    	    	                }
+    	    	        		
+    	    	        		String cleanup = "DELETE FROM login_items WHERE user='"+p.getName()+"'";
+    	    	                connection.prepareStatement(cleanup).execute();
+    	    	        		
+    	    					p.sendMessage("§aSuccess! §fWelcome back, §6"+p.getName());
+    	    					p.sendMessage(m);
+    	        		    } else p.sendMessage("§cInvalid password. §fPlease try again.");
+    	        		} else p.sendMessage("You need an account. Please register at §aamongdemons.com");
+    				} catch (SQLException e) {
+    					e.printStackTrace();
+    				} catch (NoSuchAlgorithmException e) {
+    					e.printStackTrace();
+    				}
+            	}	
+        	}
+        } else if (label.equalsIgnoreCase("kits") || label.equalsIgnoreCase("kit")) {
+        	p.sendMessage("You can get §6kits §fwith §bsouls §fat §aamongdemons.com");
+        } else if (label.equalsIgnoreCase("souls") || label.equalsIgnoreCase("s")) {
+        	if (l.isLoggedIn(p)) {
+        		if (args.length == 0) {
+	    			try {
+	    	        	connection = getConnection();
+	    	    		String sql = "SELECT souls FROM users WHERE user='"+p.getName()+"'";
+	    	    		ResultSet results;
+	    				results = connection.prepareStatement(sql).executeQuery();
+	    	    		if (results.first()) {
+	    	    			p.sendMessage("You have "+displaySouls(results.getInt(1)));
+	    	    			p.setLevel(2);
+	    	    		}
+	    			} catch (SQLException e) {
+	    				e.printStackTrace();
+	    			}
+        		} else {
+        			Boolean showError = false;
+        			
+        			if (args[0].equals("give")) {
+        				// give souls
+        				if (args.length == 3) {
+        					if (!args[1].equals(p.getName())) {
+	        					int amount = Integer.parseInt(args[2]);
+	        					if (amount > 0) {
+	        						try {
+		        						connection = getConnection();
+		        						
+		        						String sql = "SELECT souls FROM users WHERE user='"+p.getName()+"'";
+		            	        		ResultSet results = connection.prepareStatement(sql).executeQuery();
+		            	        		
+		            	        		if (results.first()) {
+		            	        			if (results.getInt(1) >= amount) {
+				        						String target_sql = "SELECT souls FROM users WHERE user='"+args[1]+"'";
+				            	        		ResultSet target_results = connection.prepareStatement(target_sql).executeQuery();
+				            	        		if (target_results.first()) {
+					        						String update = "UPDATE users SET souls = souls - "+amount+" WHERE user='"+p.getName()+"'";
+													connection.prepareStatement(update).execute();
+					        						update = "UPDATE users SET souls = souls + "+amount+" WHERE user='"+args[1]+"'";
+													connection.prepareStatement(update).execute();
+													
+					        						p.sendMessage("You gave "+displaySouls(amount)+" to §6"+args[1]+"§f");
+				            	        		} else p.sendMessage("The user §6"+args[1]+"§f does not exist");
+		            	        			} else p.sendMessage("You don't have enough §bsouls.");
+		            	        		}
+									} catch (SQLException e) {
+										e.printStackTrace();
+									}
+	        					} else p.sendMessage("[quantity] needs to be over §60");
+        					} else p.sendMessage("You can't send §bsouls§f to yourself");
+        				} else showError = true;
+        			} else if (args[0].equals("buy")) {       				
+        				if (args.length == 2) {
+	        				if (args[1].matches("\\-?\\d+")) {
+	        					int amount = Integer.parseInt(args[1]);
+	        					if (amount > 0) {
+		        					int level = p.getLevel();
+		        					level -= (amount+1);
+		        					if (level >= 0) {
+		    	    	                try {
+			        						connection = getConnection();
+			        						
+			        						String update = "UPDATE users SET souls = souls + "+amount+" WHERE user='"+p.getName()+"'";
+											connection.prepareStatement(update).execute();
+										} catch (SQLException e) {
+											e.printStackTrace();
+										}
+		        						
+		        						p.setLevel(level);
+		        						p.sendMessage("You bought "+displaySouls(amount));
+		        					} else p.sendMessage("You need §6"+(amount+1)+"§f levels to buy "+displaySouls(amount));
+	        					} else p.sendMessage("[quantity] needs to be over §60");
+	        				} else showError = true;
+        				} else showError = true;
+        			} else if (args[0].equals("sell")) {
+        				if (args.length == 2) {
+	        				if (args[1].matches("\\-?\\d+")) {
+	        					int amount = Integer.parseInt(args[1]);
+	        					if (amount > 1) {
+	    	    	                try {
+		        						connection = getConnection();
+		        						
+		        						String sql = "SELECT souls FROM users WHERE user='"+p.getName()+"'";
+		            	        		ResultSet results = connection.prepareStatement(sql).executeQuery();
+		            	        		
+		            	        		if (results.first()) {
+		            	        			if (results.getInt(1) >= amount) {
+					        					int level = p.getLevel();
+					        					level += (amount-1);
+					        					p.setLevel(level);
+				        						p.sendMessage("You sold "+displaySouls(amount)+" for §6"+(amount-1)+"§f levels");
+				        						
+				        						String update = "UPDATE users SET souls = souls - "+amount+" WHERE user='"+p.getName()+"'";
+												connection.prepareStatement(update).execute();
+		            	        			} else p.sendMessage("You don't have enough §bsouls.");
+		            	        		}
+									} catch (SQLException e) {
+										e.printStackTrace();
+									}
+	        					} else p.sendMessage("[quantity] needs to be over §61");
+	        				} else showError = true;
+        				} else showError = true;
+        			} else showError = true;
+        			
+        			if (showError)
+        				p.sendMessage(help);
+        		}
+        	} else l.loginMessage(p);
+        }
+		return true;
+    }
+    
+    public static String displaySouls(int amount) {
+    	if (amount==1)
+    		return "§6"+amount+" §bsoul§f";
+    	return "§6"+amount+" §bsouls§f";
+    }
+    
+    public static String getMD5Hash(String s) throws NoSuchAlgorithmException {
+    	String result = s;
+    	if (s != null) {
+    	    MessageDigest md = MessageDigest.getInstance("MD5"); // or "SHA-1"
+    	    md.update(s.getBytes());
+    	    BigInteger hash = new BigInteger(1, md.digest());
+    	    result = hash.toString(16);
+    	    while (result.length() < 32) { // 40 for SHA-1
+    	        result = "0" + result;
+    	    }
+    	}
+    	return result; 
+    }
+}
